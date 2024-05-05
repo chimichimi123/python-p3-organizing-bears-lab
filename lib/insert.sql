b1107d8986756d22b18238c621b88fb30909061f
Christopher Cantu <chrischimi02@gmail.com>
2024-05-05 16:07:44 -0500

    complete

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+('Bear1', 10, 'M', 'Brown', 'Calm', 1),
+('Bear2', 5, 'F', 'Black', 'Aggressive', 1),
+('Bear3', 7, 'M', 'White', 'Calm', 0),
+('Bear4', 3, 'F', 'Brown', 'Aggressive', 1),
+('Bear5', 9, 'M', 'Black', 'Calm', 0),
+('Bear6', 1, 'F', 'White', 'Aggressive', 1),
+(NULL, 2, 'M', 'Brown', 'Calm', 0),
+('Bear8', 6, 'F', 'Black', 'Aggressive', 1);
